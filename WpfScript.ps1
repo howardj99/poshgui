eb22a399357977735297271d089456f3fa5e58af
howardj99 <howard.jonathan.21@gmail.com>
2019-12-30 20:12:21 -0600

    feature(core): GUI no longer hangs during heavy processing
    Added utility function to call a control's dispatcher
    
    Refactored, borrowing pieces of template from https://github.com/1RedOne/BlogPosts/blob/master/GUI%20Part%20V/PowerShell_GUI_Template.ps1

--- WpfScript.ps1
+++ WpfScript.ps1
@@ -2,11 +2,12 @@
 #----Initial Declarations-------------------------------------#
 #-------------------------------------------------------------#
 #region
+
 Add-Type -AssemblyName PresentationCore, PresentationFramework, WindowsBase
 
-$ControlsHash = [HashTable]::Synchronized(@{})
+$Global:SyncHash = [HashTable]::Synchronized(@{})
 
-$ControlsHash.Xaml = @"
+$SyncHash.Xaml = @"
 <Window
         xmlns="http://schemas.microsoft.com/winfx/2006/xaml/presentation"
         xmlns:x="http://schemas.microsoft.com/winfx/2006/xaml"
@@ -26,26 +27,17 @@ $ControlsHash.Xaml = @"
 #endregion
 
 #-------------------------------------------------------------#
-#----Event Handlers-------------------------------------------#
-#-------------------------------------------------------------#
-#region
-
-# TO-DO: Remove runspace when script execution ends
-# $Runspace.Add_StateChanged({})
-
-#endregion
-
-#-------------------------------------------------------------#
 #----Utility Functions----------------------------------------#
 #-------------------------------------------------------------#
 #region
+
 Function New-Runspace
 {
     $Runspace = [RunspaceFactory]::CreateRunspace($Host)
     $Runspace.ApartmentState = 'STA'
     $Runspace.ThreadOptions  = 'ReuseThread'
     $Runspace.Open()
-    $Runspace.SessionStateProxy.SetVariable('ControlsHash', $ControlsHash)
+    $Runspace.SessionStateProxy.SetVariable('SyncHash', $SyncHash)
 
     Return $Runspace
 }
@@ -58,91 +50,130 @@ Function Start-Runspace
 
     $Thread = [PowerShell]::Create('NewRunspace').AddScript($ScriptBlock)
     $Thread.Runspace = $Runspace
-
+    
     Return $Thread
 }
-#endregion
 
+Function Update-Control
+{
+    [CmdletBinding()]
+    Param([Parameter(Mandatory=$True,Position=0)][String]$Name,
+          [Parameter(Mandatory=$True,Position=1)][ScriptBlock]$ScriptBlock)
+
+    $Control = $SyncHash[$Name]    
+    If($Control) { $Control.Dispatcher.Invoke($ScriptBlock) }
+}
+
+$SyncHash.NewRunspace   = ${Function:New-Runspace}
+$SyncHash.StartRunspace = ${Function:Start-Runspace}
+$SyncHash.UpdateControl = ${Function:Update-Control}
+
+#endregion
 
 #-------------------------------------------------------------#
 #----Script Execution-----------------------------------------#
 #-------------------------------------------------------------#
 #region
-$ControlsHash.NewRunspace   = ${Function:New-Runspace}
-$ControlsHash.StartRunspace = ${Function:Start-Runspace}
 
-$ConsoleScript = {
-    $WindowScript = {
-        $ControlsHash.Window = [Windows.Markup.XamlReader]::Parse($ControlsHash.Xaml)
+$WindowScript = {
+    $SyncHash.Window = [Windows.Markup.XamlReader]::Parse($SyncHash.Xaml)
 
-        $Elements = ([Xml]$ControlsHash.Xaml).GetElementsByTagName('*')
+    $Elements = ([Xml]$SyncHash.Xaml).GetElementsByTagName('*')
 
-        ForEach($Element in $Elements)
-        {
-            $Control = $ControlsHash.Window.FindName($Element.Name)
-            If($Control) { $ControlsHash.Add($Control.Name, $Control) }
-        }
-    }
-
-    # Add user events inside this script block
-    $EventScript = {        
-        Function Update-TextBox([String]$UpdateText)
-        {
-            $ControlsHash.TextBox.Dispatcher.Invoke({
-                $ControlsHash.TextBox.AppendText($UpdateText)
-            })
-        }
-        
-        $ControlsHash.UpdateTextBox = ${Function:Update-TextBox}
-
-        $ControlsHash.Button.Add_Click(
-        {
-            $this.IsEnabled = $False
-
-            &$ControlsHash.UpdateTextBox("Click!`n")
-            Start-Sleep 5
-
-            $this.IsEnabled = $True
-        })
-    }
-
-    # Dedicated runspace and thread for GUI window
-    $WindowRunspace = & $ControlsHash.NewRunspace
-    $WindowThread = & $ControlsHash.StartRunspace $WindowRunspace $WindowScript
-    $WindowThread.Invoke()
-
-    # Dedicated runspace and thread for GUI control events
-    $EventRunspace = &$ControlsHash.NewRunspace
-    $EventThread = & $ControlsHash.StartRunspace $EventRunspace $EventScript
-    $EventThread.Invoke()
-
-    # Repurpose WindowThread since it owns the Window object
-    $WindowThread.Commands.Clear()
-    [Void]$WindowThread.AddScript({ [Void]$ControlsHash.Window.ShowDialog() })
-
-    # Invoke Asynchronously
-    $WindowStatus = $WindowThread.BeginInvoke()
-    Do {<#Nothing#>} Until ($WindowStatus.IsCompleted)
-    $WindowStatus = $WindowThread.EndInvoke($WindowStatus)
-
-    # Clean up runspaces
-    $AllRunspaces = Get-Variable -Name *Runspace
-
-    ForEach($Runspace in $AllRunspaces)
+    ForEach($Element in $Elements)
     {
-        $Runspace.Value.Close()
-        $Runspace.Value.Dispose()
+        $Control = $SyncHash.Window.FindName($Element.Name)
+        If($Control) { $SyncHash.Add($Control.Name, $Control) }
     }
+
+    #region Background runspace to clean up jobs
+    $Script:JobCleanup = [HashTable]::Synchronized(@{})
+    $Script:Jobs = [System.Collections.ArrayList]::Synchronized((New-Object System.Collections.ArrayList))
+
+    $JobCleanup.Flag = $True
+    $JobsRunspace =[RunspaceFactory]::CreateRunspace()
+    $JobsRunspace.ApartmentState = "STA"
+    $JobsRunspace.ThreadOptions = "ReuseThread"          
+    $JobsRunspace.Open()        
+    $JobsRunspace.SessionStateProxy.SetVariable("JobCleanup", $JobCleanup)     
+    $JobsRunspace.SessionStateProxy.SetVariable("Jobs", $Jobs) 
+
+    $JobCleanup.PowerShell = [PowerShell]::Create().AddScript({
+        #Routine to handle completed runspaces
+        Do {    
+            Foreach($Runspace in $Jobs) {            
+                If ($Runspace.Runspace.IsCompleted) {
+                    [Void]$Runspace.Powershell.EndInvoke($Runspace.Runspace)
+                    $Runspace.Powershell.Dispose()
+                    $Runspace.Runspace = $null
+                    $Runspace.Powershell = $null               
+                } 
+            }
+            #Clean out unused runspace jobs
+            $TempHash = $Jobs.Clone()
+            $TempHash.Where{ $_.Runspace -eq $Null }.ForEach{ $Jobs.Remove($_) }
+            Start-Sleep -Seconds 1     
+        } While ($JobCleanup.Flag)
+    })
+
+    $JobCleanup.PowerShell.Runspace = $JobsRunspace
+    $JobCleanup.Thread = $JobCleanup.PowerShell.BeginInvoke()  
+    #endregion Background runspace to clean up jobs
+
+    $SyncHash.Button.Add_Click({
+        $ButtonScript = {
+            & $SyncHash.UpdateControl TextBox { $SyncHash.TextBox.AppendText("Populating list...") }
+
+            $FileList = Get-ChildItem $env:USERPROFILE\Documents -File -Recurse
+
+            & $SyncHash.UpdateControl ProgressBar {
+                write-host $FileList.Count
+                $SyncHash.ProgressBar.Value   = 0
+                $SyncHash.ProgressBar.Minimum = 0
+                $SyncHash.ProgressBar.Maximum = $FileList.Count
+            }
+
+            & $SyncHash.UpdateControl TextBox { $SyncHash.TextBox.AppendText("Done!`n") }
+
+            For($i = 0 ; $i -lt $FileList.Count ; $i++)
+            {
+                & $SyncHash.UpdateControl ProgressBar { $SyncHash.ProgressBar.Value = $i }
+                & $SyncHash.UpdateControl TextBox { $SyncHash.TextBox.AppendText("$($FileList[$i])`n") }
+            }
+        }
+
+        $ButtonRunspace = & $SyncHash.NewRunspace
+        $ButtonThread   = & $SyncHash.StartRunspace $ButtonRunspace $ButtonScript
+
+        [Void]$Jobs.Add((
+            [PSCustomObject]@{
+                PowerShell = $ButtonThread
+                Runspace = $ButtonThread.BeginInvoke()
+            }
+        ))
+    })
+
+    #region Window Close 
+    $SyncHash.Window.Add_Closed({
+        Write-Verbose 'Halt runspace cleanup job processing'
+        $JobCleanup.Flag = $False
+
+        #Stop all runspaces
+        $JobCleanup.PowerShell.Dispose()      
+    })
+    #endregion Window Close 
+    #endregion Boe's Additions
+
+    #$x.Host.Runspace.Events.GenerateEvent( "TestClicked", $x.test, $null, "test event")
+
+    #$SyncHash.Window.Activate()
+    [Void]$SyncHash.Window.ShowDialog()
+    $SyncHash.Error = $Error
 }
 
-# Dedicated runspace and thread for console host
-$ConsoleRunspace = & $ControlsHash.NewRunspace
-$ConsoleThread = & $ControlsHash.StartRunspace $ConsoleRunspace $ConsoleScript
+$WindowRunspace = & $SyncHash.NewRunspace
+$WindowThread = & $SyncHash.StartRunspace $WindowRunspace $WindowScript
 
-# Invoke Asynchronously
-$ConsoleStatus = $ConsoleThread.BeginInvoke()
-Do { <#Nothing#> } Until ($ConsoleStatus.IsCompleted)
-$ConsoleStatus = $ConsoleThread.EndInvoke($ConsoleStatus)
+$Data = $WindowThread.BeginInvoke()
 
-# Remove-Variable ControlsHash, *Runspace, *Script, *Status, *Thread
 #endregion
